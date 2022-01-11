aaf3cbf9b9e76bcf5aa7f52264d787465ff223f5
VallecillaJesus <jesusvallecilla27@gmail.com>
2022-01-11 13:02:30 -0500

    Starting my little calculator

---
+++ src/components/Calc.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {CalcDev, CalcContainer} from './styledComponents'
+
+const Calc = () => {
+    return (
+        <CalcDev>
+            <CalcContainer>
+            </CalcContainer>
+        </CalcDev>
+    )
+}
+
+export default Calc
---
+++ src/components/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const CalcDev = styled.div`
+    display:flex;
+    justify-content:center;
+    width:100%;
+    height:100%;
+    background: #525252;
+`;
+
+export const CalcContainer = styled.div`
+    width: 400px;
+    height: 570px;
+    background-color: #393E46 ;
+`;
