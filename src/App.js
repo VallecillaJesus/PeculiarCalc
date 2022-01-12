b59ed9ba5dceca88e9f5e68f650684ab5a25d2ab
VallecillaJesus <jesusvallecilla27@gmail.com>
2022-01-12 15:45:15 -0500

    Mock up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import Calc from './components/Calc';
+import {CalcDev, CalcContainer, CalcScreen,CalcButtons, Container} from './components/styledComponents';
+import CalcButton from './components/CalcButton'
 function App() {
+
+  const writeNumber = () => {
+    console.log("HOla cmo esta")
+  }
+
   return (
-    <>
-      <Calc/> 
-    </>
+    <div>
+      <CalcDev>
+        <Container>
+          <CalcContainer>
+            <CalcScreen></CalcScreen>
+            <CalcButtons>
+                {[1,2,3,'+',4,5,6,'-',7,8,9,0,'.','/','='].map((x,i) => <CalcButton onClick={() => console.log(i)} key={i} number={x}/> )}
+            </CalcButtons>
+          </CalcContainer>
+        </Container>
+      </CalcDev>
+    </div>
   );
 }
 
--- src/components/styledComponents.js
+++ src/components/styledComponents.js
@@ -1,16 +1,55 @@
 import styled from 'styled-components';
 
 
+/**
+ * 
+ * General calc styles components
+ */
 export const CalcDev = styled.div`
     display:flex;
     justify-content:center;
+    background: #525252;
     width:100%;
     height:100%;
-    background: #525252;
 `;
 
 export const CalcContainer = styled.div`
+    display:grid;
+    grid-template-rows:1 1;
+    gap:30px;
+    margin:10px;
+`;
+
+export const CalcScreen = styled.div`
+    background-color:#d8e2dc;
+    height:100px;
+    border-radius:10px;
+`;
+
+export const CalcButtons = styled.div`
+    display:grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap:10px;
+`;
+
+export const Container = styled.div`
     width: 400px;
-    height: 570px;
-    background-color: #393E46 ;
+    height: 578px;
+    background-color: #393E46;
+    border-radius:10px;
+`;
+//---------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Calculator buttons styled components
+ *
+ */
+
+export const ContentButton = styled.button`
+    border: 0;
+    height: 50px;
+    width:  75px;
+    background: #F2A649;
+    font-size:1.2em;
+    border-radius: 50px;
 `;
